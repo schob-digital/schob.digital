77ac57b5aee1151f9556d50b5b05ee5c6a1a174c
schob-digital <schobvasily.digital@gmail.com>
2026-05-11 16:50:51 +0200

    Switch site to custom root domain

--- site.config.ts
+++ site.config.ts
@@ -1,6 +1,5 @@
 export const siteName = 'Schob Digital';
 
-// Update this if the site later moves from GitHub Pages to a custom domain.
-export const siteOrigin = 'https://schob-digital.github.io';
-export const siteBasePath = '/schob.digital/';
+export const siteOrigin = 'https://schob-digital.de';
+export const siteBasePath = '/';
 export const siteBaseUrl = new URL(siteBasePath, siteOrigin).toString();
